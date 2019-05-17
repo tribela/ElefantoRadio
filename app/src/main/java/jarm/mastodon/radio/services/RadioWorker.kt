af5fb16458a57bc6d547655400c82b6197aee139
kjwon15 <kjwonmail@gmail.com>
2019-05-18 00:38:18 +0900

    WIP: use mastodon4j

--- app/src/main/java/jarm/mastodon/radio/services/RadioWorker.kt
+++ app/src/main/java/jarm/mastodon/radio/services/RadioWorker.kt
@@ -5,10 +5,16 @@ import android.os.Looper
 import android.speech.tts.TextToSpeech
 import android.util.Log
 import android.widget.Toast
-import okhttp3.*
-import org.json.JSONObject
+import com.google.gson.Gson
+import com.sys1yagi.mastodon4j.MastodonClient
+import com.sys1yagi.mastodon4j.api.Handler
+import com.sys1yagi.mastodon4j.api.Shutdownable
+import com.sys1yagi.mastodon4j.api.entity.Notification
+import com.sys1yagi.mastodon4j.api.entity.Status
+import com.sys1yagi.mastodon4j.api.exception.Mastodon4jRequestException
+import com.sys1yagi.mastodon4j.api.method.Streaming
+import okhttp3.OkHttpClient
 import org.jsoup.Jsoup
-import java.util.*
 
 class RadioWorker(
     private val context: Context,
@@ -18,51 +24,41 @@ class RadioWorker(
 ) {
 
     // TODO("Make it list")
-    private var websocket: WebSocket? = null
+    private val streamHandler = StreamHandler()
     private val mainHandler = android.os.Handler(Looper.getMainLooper())
-    private var thread: Thread
-
-    private val job: Runnable = Runnable {
-        try {
-            val domain = domain
-
-            val client = OkHttpClient()
-            var request = Request.Builder()
-                .url("https://$domain/api/v1/instance")
-                .build()
-            val response = client.newCall(request).execute()
-            val apiResult = JSONObject(response.body()?.string())
-            val streamingUrl = apiResult.getJSONObject("urls").getString("streaming_api")
-
-            val streamingPath = "$streamingUrl/api/v1/streaming/"
-            request = Request.Builder()
-                .url("$streamingPath?stream=public")
-                .addHeader("Authorization", "Bearer $accessToken")
-                .build()
-            websocket = client.newWebSocket(request!!, StreamListener())
-        } catch (e: Exception) {
-            showToast(e.message!!)
-            websocket?.cancel()
-            mainHandler.postDelayed(reconnect, 3000)
-        }
-    }
-
-    init {
-        thread = Thread(job)
-    }
+    private var shutdownable: Shutdownable? = null
+    private var thread: Thread? = null
 
     fun run() {
-        thread.start()
+        // First, Stop if running
+        shutdownable?.shutdown()
+
+        val client = MastodonClient.Builder(
+            domain,
+            OkHttpClient.Builder(),
+            Gson()
+        )
+            .accessToken(accessToken)
+            .useStreamingApi()
+            .build()
+
+        val streaming = Streaming(client)
+        Log.d("Elefanto", "Starting stream $domain")
+        thread = Thread(Runnable {
+            try {
+                shutdownable = streaming.federatedPublic(streamHandler)
+            } catch (e: Mastodon4jRequestException) {
+                // TODO: Stop service or something
+                showToast(e.message!!)
+            }
+        })
+        thread!!.start()
     }
 
     fun stop() {
-        websocket?.close(1000, null)
-    }
-
-    private val reconnect = Runnable {
-        websocket?.cancel()
-        thread = Thread(job)
-        thread.start()
+        Log.d("Elefanto", "Stopping steram $domain")
+        shutdownable?.shutdown()
+        thread?.join()
     }
 
     fun fromHtml(html: String): String {
@@ -78,62 +74,26 @@ class RadioWorker(
         }
     }
 
-    inner class StreamListener : WebSocketListener() {
-        override fun onOpen(webSocket: WebSocket, response: Response) {
-            super.onOpen(webSocket, response)
-            Log.d("Elefanto", "Websocket Open")
-            showToast("Started", Toast.LENGTH_SHORT)
+    inner class StreamHandler : Handler {
+        override fun onDelete(id: Long) {
+            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            Log.i("Elefanto", id.toString())
         }
 
-        override fun onFailure(webSocket: WebSocket, t: Throwable, response: Response?) {
-            super.onFailure(webSocket, t, response)
-            Log.e("Elefanto", "Websocket Fail $response $t")
-            showToast(t.message!!)
-            websocket?.cancel()
-            mainHandler.postDelayed(reconnect, 3000)
+        override fun onNotification(notification: Notification) {
+            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            Log.i("Elefanto", notification.id.toString())
         }
 
-        override fun onMessage(webSocket: WebSocket, text: String) {
-            super.onMessage(webSocket, text)
-            val response = JSONObject(text)
-            val event = response.getString("event")
+        override fun onStatus(status: Status) {
 
-            when (event) {
-                "update" -> handleUpdate(response)
-            }
-        }
+            // TODO: Check if boost
+            val acc = status.account?.userName!!
+            val lang = status.language
+            val content = status.content
 
-        private fun handleUpdate(response: JSONObject) {
-            val payload = JSONObject(response.getString("payload"))
-            val content = payload.getString("content")
-
-            val displayName = payload.getJSONObject("account").getString("display_name")
-            val text = fromHtml(content)
-            val lang = if (!payload.isNull("language")) payload.getString("language") else null
-            val uri = payload.getString("uri")
-            Log.d("Elefanto", content)
-            Log.i("Elefanto", "$lang $displayName: $text")
-
-
-            tts.language = tts.defaultVoice.locale
-            if (lang != null) {
-                val locale = Locale(lang)
-                if (tts.isLanguageAvailable(locale) == TextToSpeech.LANG_AVAILABLE) {
-                    tts.language = locale
-                }
-            }
-
-            tts.speak(
-                text,
-                TextToSpeech.QUEUE_ADD,
-                null,
-                uri
-            )
-        }
-
-        override fun onClosed(webSocket: WebSocket, code: Int, reason: String) {
-            super.onClosed(webSocket, code, reason)
-            Log.d("Elefanto", "Websocket Closed")
+            Log.i("Elefanto", "$acc $lang: $content")
+            // TODO:("Speak to tts")
         }
     }
 }
